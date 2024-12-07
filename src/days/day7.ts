7aab1d910a528452d29ae1f4fb74e32e0b69d9d3
Keiran Hines <keiranhines@gmail.com>
2024-12-07 16:59:49 +1100

    WiP: Day 7 part 1

---
+++ src/days/day7.ts
@@ -0,0 +1,73 @@
+import { equal } from "@std/assert/equal";
+
+export async function main() {
+	const filepath = import.meta.filename || "";
+	const parts = filepath.split("/");
+	let day = parts[parts.length - 1].split(".")[0];
+
+	const input = await Deno.readTextFile(`inputs/${day}`).catch(() => {
+		console.warn(`Could not read file inputs/${day}`);
+		Deno.exit(1);
+	}).then((i) => i.trim());
+	day = day.replace("day", "");
+	console.log(`Day ${day} part 1 answer is: `, part1(input));
+	console.log(`Day ${day} part 2 answer is: `, part2(input));
+}
+
+if (import.meta.main) {
+	await main();
+}
+
+//@ts-expect-error i said so
+function* combinationN(array: Array<number>, n: number) {
+	if (n === 1) {
+		for (const a of array) {
+			yield [a];
+		}
+		return;
+	}
+
+	for (let i = 0; i <= array.length - n; i++) {
+		//@ts-expect-error i said so
+		for (const c of combinationN(array.slice(i + 1), n - 1)) {
+			yield [array[i], ...c];
+		}
+	}
+}
+
+function findTotal(opts: Array<number>, total: number) {
+	for (let i = 1; i < opts.length; i++) {
+		for (const c of combinationN(opts, i)) {
+			const calc = opts.reduce((total, opt) => {
+				if (c.includes(opt)) {
+					if (total == 0) {
+						return 1 * opt;
+					}
+					return total * opt;
+				}
+				return total + opt;
+			}, 0);
+			if (calc == total) {
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+export function part1(input: string): number {
+	return input.split("\n").reduce((total, equation) => {
+		const [targetString, optString] = equation.split(": ");
+		const target = +targetString;
+		const opts = optString.split(" ").map((opt) => +opt);
+		if (findTotal(opts, target)) {
+			return total + target;
+		}
+		return total;
+	}, 0);
+}
+
+export function part2(input: string): number {
+	// TODO: Implement part 2 here.
+	return input.length;
+}
---
+++ src/days/day7_test.ts
@@ -0,0 +1,22 @@
+import { assertEquals } from "@std/assert";
+import { part1, part2 } from "./day7.ts";
+
+const input = `
+190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20
+	`.trim();
+
+Deno.test("Day 7 Part 1", () => {
+	assertEquals(part1(input), 3749);
+});
+
+Deno.test("Day 7 part 2", () => {
+	assertEquals(part2(input), 0);
+});
