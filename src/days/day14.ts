a0f675b76b276c5273181a44cff7eaecef3d566f
Keiran Hines <keiranhines@gmail.com>
2024-12-14 20:59:23 +1100

    Feat: Day 14 part 2 manual verification solution

--- src/days/day14.ts
+++ src/days/day14.ts
@@ -64,16 +64,42 @@ function calculateQuadrants(robots: Array<Robot>, gx: number, gy: number) {
 	return quad;
 }
 
-function printGrid(robots: Array<Robot>, gx: number, gy: number) {
+function findLine(robots: Array<Robot>, gx: number, gy: number) {
+	const yMap = new Map();
+	const xMap = new Map();
+	robots.forEach((r) => {
+		if (!yMap.has(r.y)) {
+			yMap.set(r.y, 0);
+		}
+		yMap.set(r.y, yMap.get(r.y) + 1);
+		if (!xMap.has(r.x)) {
+			xMap.set(r.x, 0);
+		}
+		xMap.set(r.x, yMap.get(r.x) + 1);
+	});
+	for (const v of yMap.values()) {
+		if (v >= gx / 4) {
+			return true;
+		}
+	}
+	for (const v of xMap.values()) {
+		if (v >= gy / 4) {
+			return true;
+		}
+	}
+	return false;
+}
+
+function printGrid(robots: Array<Robot>, gx: number, gy: number, char = ".") {
 	const midHoz = Math.floor(gy / 2);
 	const midVert = Math.floor(gx / 2);
 	const grid: Array<Array<number | string>> = [];
 	for (let y = 0; y < gy; y++) {
-		grid.push(Array(gx).fill("."));
+		grid.push(Array(gx).fill(char));
 	}
 	robots.forEach((r) => {
 		const pos = grid[r.y][r.x];
-		if (pos == ".") {
+		if (pos == char) {
 			grid[r.y][r.x] = 1;
 		} else {
 			//@ts-expect-error checked
@@ -107,7 +133,32 @@ export function part1(input: string, gx = 101, gy = 103, count = 100): number {
 	return quad.reduce((t, q) => t * q, 1);
 }
 
-export function part2(input: string): number {
-	// TODO: Implement part 2 here.
-	return input.length;
+export function part2(
+	input: string,
+	gx = 101,
+	gy = 103,
+): number {
+	const robots: Array<Robot> = [];
+	input.split("\n").forEach((line) => {
+		const parts = line.split(" ");
+		const [x, y] = parts[0].split("=")[1].split(",");
+		const [vx, vy] = parts[1].split("=")[1].split(",");
+		robots.push(new Robot(+x, +y, +vx, +vy));
+	});
+
+	let i = 0;
+	while (true) {
+		i++;
+		robots.forEach((r) => {
+			r.x += r.vx;
+			r.y += r.vy;
+		});
+		calculateFinalPositions(robots, gx, gy);
+		if (findLine(robots, gx, gy)) {
+			printGrid(robots, gx, gy, " ");
+			prompt("Run " + i);
+		}
+	}
+
+	return 0;
 }
