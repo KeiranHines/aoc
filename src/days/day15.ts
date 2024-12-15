bf87d0d8b895b030a90b8f795f7b18ca6b68f8a5
Keiran Hines <keiranhines@gmail.com>
2024-12-15 20:21:32 +1100

    Feat: Day 15 Messy solution

---
+++ src/days/day15.ts
@@ -0,0 +1,407 @@
+export async function main() {
+	const filepath = import.meta.filename || "";
+	const parts = filepath.split("/");
+	let day = parts[parts.length - 1].split(".")[0];
+
+	const input = await Deno.readTextFile(`inputs/${day}`).catch(() => {
+		console.warn(`Could not read file inputs/${day}`);
+		Deno.exit(1);
+	}).then((i) => i.trim());
+	day = day.replace("day", "");
+	//console.log(`Day ${day} part 1 answer is: `, part1(input));
+	console.log(`Day ${day} part 2 answer is: `, part2(input));
+}
+
+if (import.meta.main) {
+	await main();
+}
+class Box {
+	char: string;
+	y: number;
+	x: number;
+	constructor(char: string, y: number, x: number) {
+		this.char = char;
+		this.y = y;
+		this.x = x;
+	}
+}
+
+function boxInBoxes(boxes: Set<Box>, y: number, x: number) {
+	for (const b of boxes) {
+		if (b.x == x && b.y == y) {
+			return true;
+		}
+	}
+	return false;
+}
+function shiftUp(
+	map: Array<Array<string>>,
+	x: number,
+	y: number,
+	boxSymbols = "O",
+): boolean {
+	if (y === 0) {
+		return false;
+	}
+	if (map[y - 1][x] === "#") {
+		return false;
+	}
+	if (map[y - 1][x] === ".") {
+		map[y][x] = ".";
+		map[y - 1][x] = "@";
+		return true;
+	}
+	if (boxSymbols.includes(map[y - 1][x])) {
+		const boxes: Set<Box> = new Set();
+		if (map[y - 1][x] == "O") {
+			boxes.add(new Box("O", y - 1, x));
+		} else if (map[y - 1][x] == "[") {
+			boxes.add(new Box("[", y - 1, x));
+			boxes.add(new Box("]", y - 1, x + 1));
+		} else if (map[y - 1][x] == "]") {
+			boxes.add(new Box("[", y - 1, x - 1));
+			boxes.add(new Box("]", y - 1, x));
+		}
+		while (true) {
+			const initLength = boxes.size;
+			for (const box of boxes) {
+				const by = box.y;
+				const bx = box.x;
+				if (map[by - 1][bx] == "#") {
+					return false;
+				}
+				if (
+					boxSymbols.includes(map[by - 1][bx]) &&
+					!boxInBoxes(boxes, by - 1, bx)
+				) {
+					if (map[by - 1][bx] == "O") {
+						boxes.add(new Box("O", by - 1, bx));
+					} else if (map[by - 1][bx] == "[") {
+						boxes.add(new Box("[", by - 1, bx));
+						boxes.add(new Box("]", by - 1, bx + 1));
+					} else if (map[by - 1][bx] == "]") {
+						boxes.add(new Box("[", by - 1, bx - 1));
+						boxes.add(new Box("]", by - 1, bx));
+					}
+				}
+			}
+			if (boxes.size == initLength) {
+				break;
+			}
+		}
+
+		// Start from the top of the box stack to make moving them easier.
+		Array.from(boxes).reverse().forEach((b) => {
+			map[b.y - 1][b.x] = b.char;
+			map[b.y][b.x] = ".";
+		});
+		map[y - 1][x] = "@";
+		map[y][x] = ".";
+		return true;
+	}
+	console.log("missed a case going up");
+	return false;
+}
+function shiftDown(
+	map: Array<Array<string>>,
+	x: number,
+	y: number,
+	boxSymbols = "O",
+): boolean {
+	if (y === map.length - 1) {
+		return false;
+	}
+	if (map[y + 1][x] === "#") {
+		return false;
+	}
+	if (map[y + 1][x] === ".") {
+		map[y][x] = ".";
+		map[y + 1][x] = "@";
+		return true;
+	}
+	if (boxSymbols.includes(map[y + 1][x])) {
+		const boxes: Set<Box> = new Set();
+		if (map[y + 1][x] == "O") {
+			boxes.add(new Box("O", y + 1, x));
+		} else if (map[y + 1][x] == "[") {
+			boxes.add(new Box("[", y + 1, x));
+			boxes.add(new Box("]", y + 1, x + 1));
+		} else if (map[y + 1][x] == "]") {
+			boxes.add(new Box("[", y + 1, x - 1));
+			boxes.add(new Box("]", y + 1, x));
+		}
+		while (true) {
+			const initLength = boxes.size;
+			for (const box of boxes) {
+				const by = box.y;
+				const bx = box.x;
+				if (map[by + 1][bx] == "#") {
+					return false;
+				}
+				if (
+					boxSymbols.includes(map[by + 1][bx]) &&
+					!boxInBoxes(boxes, by + 1, bx)
+				) {
+					if (map[by + 1][bx] == "O") {
+						boxes.add(new Box("O", by + 1, bx));
+					} else if (map[by + 1][bx] == "[") {
+						boxes.add(new Box("[", by + 1, bx));
+						boxes.add(new Box("]", by + 1, bx + 1));
+					} else if (map[by + 1][bx] == "]") {
+						boxes.add(new Box("[", by + 1, bx - 1));
+						boxes.add(new Box("]", by + 1, bx));
+					}
+				}
+			}
+			if (boxes.size == initLength) {
+				break;
+			}
+		}
+
+		// Start from the top of the box stack to make moving them easier.
+		Array.from(boxes).reverse().forEach((b) => {
+			map[b.y + 1][b.x] = b.char;
+			map[b.y][b.x] = ".";
+		});
+		map[y + 1][x] = "@";
+		map[y][x] = ".";
+		return true;
+	}
+	console.log("missed a case going down", y, x);
+	map.forEach((r) => console.log(r.join("")));
+	Deno.exit(1);
+	return false;
+}
+function shiftLeft(
+	map: Array<Array<string>>,
+	x: number,
+	y: number,
+	boxSymbols = "O",
+): boolean {
+	const o = map.map((r) => [...r]);
+	if (x === 0) {
+		return false;
+	}
+	if (map[y][x - 1] === "#") {
+		return false;
+	}
+	if (map[y][x - 1] === ".") {
+		map[y][x] = ".";
+		map[y][x - 1] = "@";
+		return true;
+	}
+	if (boxSymbols.includes(map[y][x - 1])) {
+		let i = 1;
+		while (boxSymbols.includes(map[y][x - i - 1])) {
+			i++;
+		}
+		// i boxes in a row to push potentially
+		if (map[y][x - i - 1] === "#") {
+			return false;
+		}
+
+		if (map[y][x - i - 1] === ".") {
+			while (i > 0) {
+				// TODO: Replace with moving a double wide box
+				map[y][x - i - 1] = map[y][x - i];
+				i--;
+			}
+			map[y][x - 1] = "@";
+			map[y][x] = ".";
+			return true;
+		}
+	}
+	console.log("missed a case going left", y, x);
+	o.forEach((r, i) =>
+		console.log(String(i).padStart(2, "0"), ":", r.join(""))
+	);
+	map.forEach((r, i) =>
+		console.log(String(i).padStart(2, "0"), ":", r.join(""))
+	);
+	Deno.exit(1);
+	return false;
+}
+function shiftRight(
+	map: Array<Array<string>>,
+	x: number,
+	y: number,
+	boxSymbols = "O",
+): boolean {
+	if (x === 0) {
+		return false;
+	}
+	if (map[y][x + 1] === "#") {
+		return false;
+	}
+	if (map[y][x + 1] === ".") {
+		map[y][x] = ".";
+		map[y][x + 1] = "@";
+		return true;
+	}
+	if (boxSymbols.includes(map[y][x + 1])) {
+		let i = 1;
+		while (boxSymbols.includes(map[y][x + i + 1])) {
+			i++;
+		}
+		// i boxes in a row to push potentially
+		if (map[y][x + i + 1] === "#") {
+			return false;
+		}
+
+		if (map[y][x + i + 1] === ".") {
+			while (i > 0) {
+				map[y][x + i + 1] = map[y][x + i];
+				i--;
+			}
+			map[y][x + 1] = "@";
+			map[y][x] = ".";
+			return true;
+		}
+	}
+	console.log("missed a case going right");
+	return false;
+}
+
+function simulate(
+	map: Array<Array<string>>,
+	start: Array<number>,
+	moves: Array<string>,
+	boxSymbols = "O",
+) {
+	let x = start[1];
+	let y = start[0];
+	moves.forEach((m) => {
+		const initBoxCount = map.reduce(
+			(t, r) => t + r.filter((c) => boxSymbols.includes(c)).length,
+			0,
+		);
+		const o = map.map((r) => [...r]);
+		switch (m) {
+			case "^":
+				if (shiftUp(map, x, y, boxSymbols)) {
+					y--;
+				}
+				break;
+			case "v":
+				if (shiftDown(map, x, y, boxSymbols)) {
+					y++;
+				}
+				break;
+			case "<":
+				if (shiftLeft(map, x, y, boxSymbols)) {
+					x--;
+				}
+				break;
+			case ">":
+				if (shiftRight(map, x, y, boxSymbols)) {
+					x++;
+				}
+				break;
+			case "\n":
+				break;
+			default:
+				console.log("error default case", m);
+		}
+		const fBoxCount = map.reduce(
+			(t, r) => t + r.filter((c) => boxSymbols.includes(c)).length,
+			0,
+		);
+		const atCount = map.reduce(
+			(t, r) => t + r.filter((c) => c == "@").length,
+			0,
+		);
+		if (map[y][x] != "@") {
+			console.log("start position isn't where it should be");
+			o.forEach((r, i) =>
+				console.log(String(i).padStart(2, "0"), ":", r.join(""))
+			);
+			console.log("Move", m, ":", x, y, ":");
+			map.forEach((r, i) =>
+				console.log(String(i).padStart(2, "0"), ":", r.join(""))
+			);
+			Deno.exit(1);
+		}
+		if (atCount > 1) {
+			console.log("Too many start positions");
+			o.forEach((r, i) =>
+				console.log(String(i).padStart(2, "0"), ":", r.join(""))
+			);
+			console.log("Move", m, ":", x, y, ":");
+			map.forEach((r, i) =>
+				console.log(String(i).padStart(2, "0"), ":", r.join(""))
+			);
+			Deno.exit(1);
+		}
+		if (initBoxCount != fBoxCount) {
+			console.log("a Box went missing");
+			o.forEach((r, i) =>
+				console.log(String(i).padStart(2, "0"), ":", r.join(""))
+			);
+			console.log("Move", m, ":", x, y, ":");
+			map.forEach((r, i) =>
+				console.log(String(i).padStart(2, "0"), ":", r.join(""))
+			);
+			Deno.exit(1);
+		}
+	});
+}
+
+export function part1(input: string): number {
+	const [mapInput, moveInputs] = input.split("\n\n");
+	let pos: Array<number> = [];
+	const map = mapInput.split("\n").map((row, index) => {
+		const startIndex = row.indexOf("@");
+		if (startIndex > -1) {
+			pos = [index, startIndex];
+		}
+		return row.split("");
+	});
+	simulate(map, pos, moveInputs.split(""));
+	const boxes: Array<Array<number>> = [];
+	map.forEach((row, rowIndex) => {
+		row.forEach((char, index) => {
+			if (char == "O") {
+				boxes.push([rowIndex, index]);
+			}
+		});
+	});
+	return boxes.reduce((t, box) => t + (box[0] * 100 + box[1]), 0);
+}
+
+export function part2(input: string): number {
+	const [mapInput, moveInputs] = input.split("\n\n");
+	const map = mapInput.split("\n").map((row) => {
+		return row.split("").flatMap((c) => {
+			switch (c) {
+				case "#":
+					return ["#", "#"];
+				case "O":
+					return ["[", "]"];
+				case ".":
+					return [".", "."];
+				case "@":
+					return ["@", "."];
+			}
+			return c;
+		});
+	});
+	let pos: Array<number> = [];
+	for (const ri in map) {
+		const row = map[ri];
+		for (const ci in row) {
+			if (row[ci] == "@") {
+				pos = [+ri, +ci];
+			}
+		}
+	}
+	simulate(map, pos, moveInputs.split(""), "[]");
+	const boxes: Array<Array<number>> = [];
+	map.forEach((row, rowIndex) => {
+		row.forEach((char, index) => {
+			if (char == "[") {
+				boxes.push([rowIndex, index]);
+			}
+		});
+	});
+	return boxes.reduce((t, box) => t + (box[0] * 100 + box[1]), 0);
+}
---
+++ src/days/day15_test.ts
@@ -0,0 +1,62 @@
+import { assertEquals } from "@std/assert";
+import { part1, part2 } from "./day15.ts";
+
+const input = `
+##########
+#..O..O.O#
+#......O.#
+#.OO..O.O#
+#..O@..O.#
+#O#..O...#
+#O..O..O.#
+#.OO.O.OO#
+#....O...#
+##########
+
+<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+>^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^`
+	.trim();
+
+Deno.test("Day 15 Part 1", () => {
+	assertEquals(part1(input), 10092);
+});
+
+Deno.test("Day 15 Simple Part 1", () => {
+	const simple = `
+########
+#..O.O.#
+##@.O..#
+#...O..#
+#.#.O..#
+#...O..#
+#......#
+########
+
+<^^>>>vv<v>>v<<
+`.trim();
+	assertEquals(part1(simple), 2028);
+});
+
+Deno.test("Day 15 part 2", () => {
+	assertEquals(part2(input), 9021);
+});
+
+const realInput = await Deno.readTextFile(`inputs/day15`).catch(() => {
+	return "";
+}).then((i) => i.trim());
+
+Deno.bench("Day 15 part 1", () => {
+	part1(realInput);
+});
+
+Deno.bench("Day 15 part 2", () => {
+	part2(realInput);
+});
