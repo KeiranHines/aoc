39a02c9b7d20d9f5cde49ffc4b9147bbd6dc69da
Keiran Hines <keiran.hines@conserveitiot.com>
2025-12-09 09:56:18 +1100

    WiP: Still broken

--- src/bin/day8.rs
+++ src/bin/day8.rs
@@ -14,10 +14,11 @@ impl fmt::Debug for Pos {
     }
 }
 
+#[derive(Clone)]
 struct Connection {
     pos1: Pos,
     pos2: Pos,
-    distance: i32,
+    distance: f64,
 }
 
 impl fmt::Debug for Connection {
@@ -26,7 +27,7 @@ impl fmt::Debug for Connection {
     }
 }
 
-impl PartialEq for Connection {
+/*impl PartialEq for Connection {
     fn eq(&self, other: &Self) -> bool {
         let direct_match = self.pos1 == other.pos1 && self.pos2 == other.pos2;
         let alt_match = self.pos1 == other.pos2 && self.pos2 == other.pos1;
@@ -34,10 +35,14 @@ impl PartialEq for Connection {
     }
 }
 impl Eq for Connection {}
-
-fn distance_to(pos1: Pos, pos2: Pos) -> i32 {
+*/
+fn distance_to(pos1: Pos, pos2: Pos) -> f64 {
     // No euclidian distance, sqrt == float == bad
-    (pos1.x - pos2.x).pow(2) + (pos1.y - pos2.y).pow(2) + (pos1.z - pos2.z).pow(2)
+    let x = (((pos1.x - pos2.x).pow(2) + (pos1.y - pos2.y).pow(2) + (pos1.z - pos2.z).pow(2))
+        as f64)
+        .sqrt();
+    //print!("{x} ");
+    x
     //((pos1.x - pos2.x).abs() + (pos1.y - pos2.y).abs() + (pos1.z - pos2.z).abs()) as u64
 }
 
@@ -47,6 +52,7 @@ pub fn part1(input: &str) -> u64 {
 
 fn part1_processing(input: &str, limit: u32) -> u64 {
     let mut boxes: Vec<Pos> = Vec::with_capacity(1_000);
+    let mut circuits: Vec<Vec<Pos>> = Vec::with_capacity(1_000);
     let mut connections: Vec<Connection> = Vec::with_capacity(limit as usize);
     let mut dist;
     for line in input.lines() {
@@ -58,6 +64,7 @@ fn part1_processing(input: &str, limit: u32) -> u64 {
         };
 
         boxes.push(pos);
+        circuits.push(vec![pos]);
     }
 
     for (i, b1) in boxes.iter().enumerate() {
@@ -72,48 +79,46 @@ fn part1_processing(input: &str, limit: u32) -> u64 {
         }
     }
     // All possible connections found
+    //let mut cp: Vec<Connection> = connections.clone();
+    //cp.sort_by(|a, b| b.distance.total_cmp(&a.distance));
 
-    connections.sort_unstable_by_key(|a| a.distance);
+    //connections.sort_by(|a, b| b.distance.total_cmp(&a.distance));
+    connections.sort_by(|a, b| a.distance.total_cmp(&b.distance));
     connections.reverse();
-
+    /*   for i in 0..connections.len() {
+        if cp[i] != connections[i] {
+            println!("Mismatch {i} {:?} {:?}", cp[i], connections[i]);
+            return 0;
+        }
+    }*/
     // All possible connections are now sorted
-
-    let mut circuits: Vec<Vec<Pos>> = Vec::new();
     let mut c: Connection;
     for _i in 0..limit {
-        //let _ = connections.pop(); // I wrote bad code so I really only need every second
         c = connections.pop().unwrap();
         let mut c1 = None;
         let mut c2 = None;
-        for (i, circ) in &mut circuits.iter().enumerate() {
+        for (j, circ) in &mut circuits.iter().enumerate() {
             if circ.contains(&c.pos1) {
-                c1 = Some(i);
+                c1 = Some(j);
             }
             if circ.contains(&c.pos2) {
-                c2 = Some(i);
+                c2 = Some(j);
             }
         }
-        if c1 == None && c2 == None {
-            circuits.push(vec![c.pos1, c.pos2]);
-        } else if c2 == None {
-            // Only c1 is set
-            circuits[c1.unwrap()].push(c.pos2);
-        } else if c1 == None {
-            // Only c2 is set
-            circuits[c2.unwrap()].push(c.pos1);
-        } else if c1.unwrap() != c2.unwrap() {
-            let remove = cmp::max(c1.unwrap(), c2.unwrap());
-            let keep = cmp::min(c1.unwrap(), c2.unwrap());
+        let c1 = c1.unwrap();
+        let c2 = c2.unwrap();
+        if c1 != c2 {
+            let remove = cmp::max(c1, c2);
+            let keep = cmp::min(c1, c2);
             let other = circuits.remove(remove);
             circuits[keep].extend(other);
         }
-        println!("It: {_i} {}", circuits.len());
     }
 
     circuits.sort_by(|a, b| b.len().cmp(&a.len()));
     println!(
-        "Circuits {:?}",
-        circuits.iter().map(|c| c.len()).collect::<Vec<usize>>()
+        "Circuits len {:?}",
+        circuits.iter().map(|c| c.len()).collect::<Vec<usize>>(),
     );
     (circuits[0].len() * circuits[1].len() * circuits[2].len()) as u64
 }
@@ -135,22 +140,6 @@ pub fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Part 1: {p1}, Part 2: {p2}");
     println!("Part 1: {p1t:?}, Part 2: {p2t:?}");
     Ok(())
-}
-
-#[test]
-fn test_closest() {
-    let p1 = Pos {
-        x: 162,
-        y: 817,
-        z: 812,
-    };
-    let p2 = Pos {
-        x: 425,
-        y: 690,
-        z: 689,
-    };
-
-    assert_eq!(distance_to(p1, p2), 100427);
 }
 
 #[test]
