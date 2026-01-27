1a252edb166238bcb55aeae33ba3e5fefce9f435
Keiran Hines <keiranhines@gmail.com>
2026-01-27 11:43:56 +1100

    Feat: Day 12 part 2 in rust

--- rust/src/bin/day12/main.rs
+++ rust/src/bin/day12/main.rs
@@ -1,6 +1,7 @@
 use std::{env, fs, time::Instant};
 
 use regex::Regex;
+use serde_json::Value;
 
 fn part_1(content: &str) -> i32 {
     let re = Regex::new(r"(-?\d+)").unwrap();
@@ -8,8 +9,30 @@ fn part_1(content: &str) -> i32 {
         .filter_map(|m| m.as_str().parse::<i32>().ok())
         .sum()
 }
-fn part_2(_content: &str) -> i16 {
-    0
+
+fn parse_json(data: &Value) -> i64 {
+    let mut total = 0;
+    if data.is_array() {
+        for d in data.as_array().unwrap() {
+            total += parse_json(d);
+        }
+    } else if data.is_number() {
+        total += data.as_i64().unwrap()
+    } else if data.is_object() {
+        let d = data.as_object().unwrap();
+        let vals = d.values();
+        if !vals.clone().any(|v| v.as_str() == Some("red")) {
+            for v in vals {
+                total += parse_json(v);
+            }
+        }
+    }
+    total
+}
+
+fn part_2(content: &str) -> i64 {
+    let data = serde_json::from_str(content).unwrap();
+    parse_json(&data)
 }
 
 pub fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -32,4 +55,32 @@ pub fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     println!("Part 1: {p1} {p1t:?}, Part 2: {p2} {p2t:?} | Total: {t:?}");
     Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use serde_json::json;
+    #[test]
+    fn test_parse_json() {
+        let j = json!([1, 2, 3]);
+        assert_eq!(parse_json(&j), 6);
+        let j = json!([1,{"c":"red","b":2},3]);
+        assert_eq!(parse_json(&j), 4);
+        let j = json!({"d":"red","e":[1,2,3,4],"f":5});
+        assert_eq!(parse_json(&j), 0);
+        let j = json!([1, "red", 5]);
+        assert_eq!(parse_json(&j), 6);
+    }
+
+    #[test]
+    fn test_advanced() {
+        let j = json!({
+            "a": 1,
+            "b": [1, 2, "red"],
+            "c": {"a": "red", "b": 100},
+            "d": {"a":1, "b":2, "c": {"a": 1, "b":2, "c": {"a": 1000, "b":"red"}}}
+        });
+        assert_eq!(parse_json(&j), 10);
+    }
 }
